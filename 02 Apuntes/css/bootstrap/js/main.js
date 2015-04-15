2d555e55357457a1e53ef94f8df48aa41228ef83
Bienvenido Sáez Muelas <bienvenidosaez@gmail.com>
2015-04-15 12:14:24 +0200

    Carrusel con caption

--- 02 Apuntes/css/bootstrap/js/main.js
+++ 02 Apuntes/css/bootstrap/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
+  //Inicialización tooltip
   $('[data-toggle="tooltip"]').tooltip();
+
+  //Inicialización popover
+  $('#miImagen').popover();
+
+  //Inicialización del carrusel
+  $('#miCarrusel').carousel();
+
 });
